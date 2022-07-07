938ec245c18ca05e188032b80c844d5eded8d1e9
manlikasee <100658593+manlikasee@users.noreply.github.com>
2022-07-07 12:16:30 +0700

    Quiz Week 2

---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,3 @@
+<h1>Manlika 63122420010 </h1>
+<h1> <img src="{{$cat}}" >
+    <h1>cat</h1>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+    <h1>Manlika 63122420010</h1>
+    <a href="gallery/ant" ><img src="{{$ant}}" </a>
+    <h1>ant</h1>
+    <a href="gallery/bird" ><img src="{{$bird}}" </a>
+    <h1>bird</h1>
+    <a href="gallery/cat" ><img src="{{$cat}}" </a>
+    <h1>cat</h1>
+    <h1> <img src="{{$god}}" >
+    <h1>god</h1>
+    <h1> <img src="{{$spider}}">
+    <h1>spider</h1>
+</body>
+</html>
+
+
+
