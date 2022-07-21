a72c51c2d7540ef112d2ddebadecdc1491c74e89
manlikasee <100658593+manlikasee@users.noreply.github.com>
2022-07-21 12:47:34 +0700

    week 3

--- routes/web.php
+++ routes/web.php
@@ -100,3 +100,38 @@ Route::get("/gallery/cat", function () {
     return view("test/cat", compact("cat"));
 });
 
+
+
+Route::get("/teacher", function () {
+    return view("teacher");
+});
+
+Route::get("/student", function () {
+    return view("student");
+});
+
+Route::get("/theme", function () {
+    return view("theme");
+});
+
+
+// Route Template Inheritance
+Route::get("/teacher/inheritance", function () {
+    return view("teacher-inheritance");
+});
+Route::get("/student/inheritance", function () {
+    return view("student-inheritance");
+});
+
+
+// Route Template Component
+Route::get("/teacher/component", function () {
+    return view("teacher-component");
+});
+Route::get("/student/component", function () {
+    return view("student-component");
+});
+
+Route::get('/tables', function () {
+    return view('tables');
+});
