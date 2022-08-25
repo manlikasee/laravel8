b5ab2f8e20f4dcb1eda1446c8a9caf845f7b25f7
manlikasee <100658593+manlikasee@users.noreply.github.com>
2022-08-25 11:15:09 +0700

    week 7

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Covid19Controller;
 use App\Http\Controllers\MyProfileController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -162,5 +163,15 @@ Route::post("/covid19",[ Covid19Controller::class , "store" ]);
 Route::patch("/covid19/{id}", [ Covid19Controller::class , "update" ]);
 Route::delete('/covid19/{id}', [ Covid19Controller::class , 'destroy' ]);
 
+
+
+
+Route::get('/staff', [ StaffController::class, 'index' ]);
+Route::get("/staff/create",[ StaffController::class , "create" ]);
+Route::get('/staff/{id}',[ StaffController::class,'show' ]);
+Route::get("/staff/{id}/edit", [ StaffController::class , "edit" ]);
+Route::post("/staff",[ StaffController::class , "store" ]);
+Route::patch("/staff/{id}", [ StaffController::class , "update" ]);
+Route::delete('/staff/{id}', [ StaffController::class , 'destroy' ]);
 
 
