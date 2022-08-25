3b821b82d4171bdc53b564a0e8e7475045eb3506
manlikasee <100658593+manlikasee@users.noreply.github.com>
2022-08-25 11:20:45 +0700

    week7.1

--- resources/views/staff/show.blade.php
+++ resources/views/staff/show.blade.php
@@ -1,25 +1,32 @@
-@extends('bootstrap-theme')
+<x-theme title="">
+    <h1>Staff #{{ $staff->id }}</h1>
+    <table class="table table-sm" style="width:50%">
+        <tbody>
+            <tr>
+                <th> ID </th>
+                <td>{{ $staff->id }}</td>
+            </tr>
+            <tr>
+                <th> Name </th>
+                <td> {{ $staff->name }} </td>
+            </tr>
+            <tr>
+                <th> Age </th>
+                <td> {{ $staff->age }} </td>
+            </tr>
+            <tr>
+                <th> Salary </th>
+                <td> {{ $staff->salary }} </td>
+            </tr>
+            <tr>
+                <th> Phone </th>
+                <td> {{ $staff->phone }} </td>
+            </tr>
+            <tr>
+                <th> Remark </th>
+                <td> {{ $staff->remark }} </td>
+            </tr>
+        </tbody>
+    </table>
+</x-theme>
 
-@section('content')
-<h1>Staff #{{ $staff->id }}</h1>
-<table class="table table-sm" style="width:50%">
-    <tbody>
-        <tr>
-            <th> Name </th>
-            <td> {{ $staff->name }} </td>
-        </tr>
-        <tr>
-            <th> Age </th>
-            <td> {{ $staff->age }} </td>
-        </tr>
-        <tr>
-            <th> Salary</th>
-            <td> {{ $staff->salary }} </td>
-        </tr>
-        <tr>
-            <th> Phone</th>
-            <td> {{ $staff->phone}} </td>
-        </tr>
-    </tbody>
-</table>
-@endsection
